30f8ff13d10829faf009923c28e2c073f09c276b
עמדה 48 <dadash14125@gmail.com>
2022-05-17 10:41:40 +0300

    srhdgt

---
+++ Answers.py
@@ -0,0 +1,43 @@
+# for question 1
+
+
+
+myObejct = {
+    'name' : 'Alon',
+     'age' : 12,
+    'bith date': '1975/01/05'
+
+}
+
+print(myObejct)
+
+
+
+# for question 2
+myObejct = {
+    'name' : 'Alon',
+     'age' : 12,
+    'bith date': '1975/01/05'
+
+}
+
+myObejct['lastName'] = 'vilces'
+
+print(myObejct)
+
+
+
+# for question 3
+
+
+myObejct = {
+    'name' : 'Alon',
+     'age' : 12,
+    'bith date': '1975/01/05'
+
+}
+
+myObejct['Hobbies'] = ['']
+
+
+
